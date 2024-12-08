f4c2503fc0aa2a43b5555f3981bc2b99863ef8e1
Kyle Baker <kyle.baker@sinclair.edu>
2024-12-08 16:24:19 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,64 @@
+/*
+Name: Kyle Baker
+Date: 12/08/2024
+Project Name: CIS 1202 Programming Assignment #15a - Exceptions
+Project Description: Write a function to calculate character offsets, throwing exceptions in error conditions.
+*/
+
+// libraries
+#include<cmath>
+#include<fstream>
+#include<iomanip>
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+// namespace
+using namespace std;
+
+// function prototypes
+
+void emptyFunction();
+
+// classes
+
+class emptyClass {
+private:
+	int emptyPrivateNum_01;
+	string emptyPrivateString_01;
+public:
+	int emptyPublicNum_01;
+	string emptyPublicString_01;
+};
+
+	//											+++Start Main+++
+
+int main() {
+
+	// declare variables
+
+
+
+
+
+
+
+
+	//											+++END MAIN+++
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+
+}
+
+// function definitions
+
+void emptyFunction()
+{
+	
+}
+
+// class definitions
+
